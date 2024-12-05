be046735ecfd8986cc9babfbaf970fb48715a90a
Baswada Sujana <147164797+Sujana19-10@users.noreply.github.com>
2024-12-05 19:29:27 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // config.js
 export const CONFIG = {
     // Backend API URL (Main)
-    API_BASE_URL: "http://your-production-api-url.com",  // Production URL
+    API_BASE_URL: "https://codequest-backend-yuuw.onrender.com",  // Production URL
     API_BASE_URL_LOCAL: "http://localhost:3001",  // Local development URL
 
     // User Code Validation API URL
-    VALIDATION_URL: "http://your-validation-api-url.com",  // Production validation URL
+    VALIDATION_URL: "https://python-backend-3e1j.onrender.com/verify",  // Production validation URL
     VALIDATION_URL_LOCAL: "http://localhost:5000"  // Local validation URL
 };
